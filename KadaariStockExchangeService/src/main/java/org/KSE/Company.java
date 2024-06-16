34e3992d88b30ac21414474e5d5302c3dcf42820
Nitya Chandra <nck@Nityas-Laptop.local>
2024-06-16 22:37:14 +0530

    added class specifications

--- KadaariStockExchangeService/src/main/java/org/KSE/Company.java
+++ KadaariStockExchangeService/src/main/java/org/KSE/Company.java
@@ -1,4 +1,18 @@
 package org.KSE;
 
 public class Company {
+
+    String companyName;
+    Integer totalShares;
+    Float initialSharePrice;
+    Float initialCompanyWorth;
+
+    Company(String name, Integer shares, Float perSharePrice, Float companyWorth){
+        this.companyName = name;
+        this.totalShares = shares;
+        this.initialSharePrice = perSharePrice;
+        this.initialCompanyWorth = companyWorth;
+    }
+
+
 }
--- KadaariStockExchangeService/src/main/java/org/KSE/KadaariStockExchange.java
+++ KadaariStockExchangeService/src/main/java/org/KSE/KadaariStockExchange.java
@@ -1,8 +1,23 @@
 package org.KSE;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class KadaariStockExchange {
     List<Investor> investorList;
     List<Company> companyList;
+
+    KadaariStockExchange(){
+        this.investorList = new ArrayList<Investor>();
+        this.companyList = new ArrayList<Company>();
+    }
+
+    public String registerCompany(Company company) {
+        companyList.add(company);
+        return "Registered";
+    }
+
+    public String registerInvestor(Investor investor){
+        this.investorList.add(investor);
+        return "Registered";
+    }
 }
